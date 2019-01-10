09c90b0cd3c49f0bd55edfe290821dd75df29af9
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-10 21:38:21 +0100

    startup with selenium + chandling static dropdown

---
+++ src/StartupWithSelenium.java
@@ -0,0 +1,60 @@
+// all classes with .selenium are loaded from jars
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartupWithSelenium {
+
+    public static void main(String[] args) {
+
+        // 4. download Third Party Browser Drivers for proper OS and set properties
+
+        // System.setProperty("key value", "third party browser driver path")
+
+        // webdriver.chrome.driver - key value for Chrome browser
+        // webdriver.gecko.driver - key value for Firefox browser
+        // webdriver.ie.driver - key value for Internet Explorer browser
+
+        // invoke .exe file first
+
+        System.setProperty("webdriver.chrome.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/chromedriver");
+
+        // 1. download selenium client version for java from
+        // 2. https://www.seleniumhq.org/download/
+        // 3. and add all jar files to project
+
+        // create ChromeDriver object for chrome browser
+        // but to have all methods from interface WebDriver object should have type WebDriver
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.phptravels.net/"); // get this url on the browser
+
+        System.out.println(driver.getTitle()); // print title of current open page
+
+        // I can validate if I landed on correct url, check url for security purpose
+        System.out.println(driver.getCurrentUrl()); // print url from current open page
+
+        // use when right click then view page source is unavailable in browser (bank websites)
+        System.out.println(driver.getPageSource()); // print page source from current open page
+
+
+        // ---- browser navigate section ----
+
+        driver.get("https://www.google.com/"); // open new url on the same browser card (tab)
+
+        driver.navigate().back(); // click left arrow (navigate back) in the browser
+
+        driver.navigate().forward(); // click right arrow (navigate forward) in the browser
+
+        driver.navigate().refresh(); // refresh current page
+
+        driver.close(); // close current browser
+
+        driver.quit(); // close all browsers opened by selenium script
+
+
+
+
+    }
+}
---
+++ src/StaticDropdown.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.phptravels.net/");
+
+        driver.manage().window().maximize();  // maximize browser window
+
+        driver.findElement(By.xpath("//a[@title='Flights']")).click();
+
+        // Select is special class for handling static dropdown menu (with html select tag)
+        // apply all methods from Select class for found dropdown menu
+
+        Select reservationClassesMenu = new Select(driver.findElement(By.xpath("//select[@name='cabinclass']")));
+
+        //reservationClassesMenu.selectByValue("business"); // select with attribute value="business"
+
+        //reservationClassesMenu.selectByIndex(3); // select third value form menu counting from 0
+
+        reservationClassesMenu.selectByVisibleText("Business");
+
+        // deselect() methods are useful when menu have multiple options to select and I want to deselect some option(s)
+
+    }
+}

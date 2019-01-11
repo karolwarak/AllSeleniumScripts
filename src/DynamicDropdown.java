02884c56412baff3715b0a7d45aa41bc5261e2b7
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-11 21:32:30 +0100

    handling dynamic dropdown by xpath with index

---
+++ src/DynamicDropdown.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DynamicDropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.spicejet.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+
+        // without waiting script clicked very fast on 2 options but only from first dropdown menu
+        // and it fill both *from and *to box
+        // that is NOT how normally user fill this box
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//a[@value='MAA'])[2]"))));
+
+        // used index because xpath found 2 matching nodes on page. I need only second one
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+    }
+}

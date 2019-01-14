211d32dcb7cbe9f0bfb1acc3db82b89813b6bb72
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-14 21:47:49 +0100

    select radiobutton with using id

--- src/_5_RadioButton.java
+++ src/_5_RadioButton.java
@@ -1,5 +1,5 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class _5_RadioButton {
@@ -8,9 +8,13 @@ public class _5_RadioButton {
 
         System.setProperty("webdriver.gecko.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/geckodriver-v0.23.0-linux64/geckodriver");
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.makemytrip.com/");
+        driver.get("https://www.spicejet.com/");
         driver.manage().window().maximize();
 
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+        // handling radiobutton when I don't have exact locator
+        System.out.println(driver.findElements(By.xpath("//input[@name='ctl00$mainContent$rbtnl_Trip']")).size()); // count how many inputs (radio buttons) are with this name
 
     }
 }

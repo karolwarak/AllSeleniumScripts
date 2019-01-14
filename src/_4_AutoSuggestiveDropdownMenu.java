07399fecf05fa945b81f5ca3e375aa33ca8ba09e
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-14 20:48:34 +0100

    choicing city Deline in the field to

--- src/_4_AutoSuggestiveDropdownMenu.java
+++ src/_4_AutoSuggestiveDropdownMenu.java
@@ -18,13 +18,23 @@ public class _4_AutoSuggestiveDropdownMenu {
         WebElement fromField = driver.findElement(By.id("hp-widget__sfrom"));
 
         fromField.clear(); // wipeout all default data in this field
-        fromField.sendKeys("mum");
+        fromField.sendKeys("Mum");
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         //wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//li[text()='Search Result']"))));
+
+        // item Search Result appear in the list after typing mum
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[text()='Search Result']")));
 
         fromField.sendKeys(Keys.ENTER); // Keys is enum with all keyboard event
 
+        WebElement toField = driver.findElement(By.id("hp-widget__sTo"));
+        toField.clear();
+        toField.sendKeys("Del");
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//li[text()='Search Result'])[2]")));
+        // or (the same effect)
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='locationFilter autocomplete_to']//li[text()='Search Result']")));
+        toField.sendKeys(Keys.ARROW_DOWN);
+        toField.sendKeys(Keys.ENTER);
     }
 }

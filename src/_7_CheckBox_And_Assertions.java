8d7de4a93b8f9bcf791d07f9d48833bd1410b5f4
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-15 19:49:00 +0100

    added assertion checks

---
+++ src/_7_CheckBox_And_Assertions.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.assertj.core.api.Assertions.*;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
+
+
+public class _7_CheckBox_And_Assertions {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/chromedriver_linux64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.spicejet.com/");
+        driver.manage().window().maximize();
+
+        WebElement familyAndFriendsCheckbox = driver.findElement(By.cssSelector("input[name*='_friendsandfamily']"));
+
+        //System.out.println(familyAndFriendsCheckbox.isSelected()); // return boolean value of checkbox status
+
+        // AssertJ assertion
+        //assertThat(false).as("Checkbox is selected").isEqualTo(familyAndFriendsCheckbox.isSelected());
+
+        //JUnit assertion - krotsza niz w AssertJ
+        assertFalse("Checkbox is selected", familyAndFriendsCheckbox.isSelected());
+
+        familyAndFriendsCheckbox.click();
+
+        //System.out.println(familyAndFriendsCheckbox.isSelected());
+        assertTrue("Checkbox is not selected", familyAndFriendsCheckbox.isSelected());
+
+        int numberOfCheckboxes = driver.findElements(By.xpath("//input[contains(@name, 'ctl00$mainContent$chk_')]")).size();
+        assertThat(6).as("There is no 6 checkbox on web page").isEqualTo(numberOfCheckboxes);
+    }
+}

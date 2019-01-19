b501e66d60f96a8263c6e6c6cf467872f9859177
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-19 16:40:21 +0100

    select specific month and day in calendar

---
+++ src/_1_Calendar_SelectWithDay.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class _1_Calendar_SelectWithDay {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/geckodriver-v0.23.0-linux64/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.path2usa.com/travel-companions");
+        driver.manage().window().maximize();
+
+        //select January 29
+
+        driver.findElement(By.id("travel_date")).click();
+
+        // logic -> 1) grab common attribute 2) put into list 3) iterate
+        List<WebElement> days = driver.findElements(By.className("day"));
+        int count = days.size();
+
+        for(int i=0; i<count; i++){
+
+            String dayOfMonth = days.get(i).getText();
+            if(dayOfMonth.equals("20")){
+                days.get(i).click();
+                break;
+            }
+        }
+    }
+}
--- src/_2_Calendar_SelectWithDayAndMonth.java
+++ src/_2_Calendar_SelectWithDayAndMonth.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.List;
 
-public class _1_Calendar_SelectAnyDate {
+public class _2_Calendar_SelectWithDayAndMonth {
 
     public static void main(String[] args) {
 
@@ -18,13 +18,18 @@ public class _1_Calendar_SelectAnyDate {
 
         driver.findElement(By.id("travel_date")).click();
 
+        while(!(driver.findElement(By.xpath("//div[@class='datepicker-days']//th[@class='datepicker-switch']"))).getText().contains("April 2019")){
+                driver.findElement(By.xpath("//div[@class='datepicker-days']//th[@class='prev']")).click();
+        }
+
+        // logic -> 1) grab common attribute 2) put into list 3) iterate
         List<WebElement> days = driver.findElements(By.className("day"));
         int count = days.size();
 
         for(int i=0; i<count; i++){
 
             String dayOfMonth = days.get(i).getText();
-            if(dayOfMonth.equals("20")){
+            if(dayOfMonth.equals("6")){
                 days.get(i).click();
                 break;
             }

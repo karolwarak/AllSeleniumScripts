1816e60c20bb33d67ad5a877be38c96932a1ad40
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-22 06:09:20 +0100

    open 6 links in separate tabs

--- src/_4_practical_things_with_selenium/_1_CountLinksInThePage.java
+++ src/_4_practical_things_with_selenium/_1_CountLinksInThePage.java
@@ -1,9 +1,12 @@
 package _4_practical_things_with_selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class _1_CountLinksInThePage {
 
@@ -19,10 +22,29 @@ public class _1_CountLinksInThePage {
         // print number of all links on the page
         System.out.println(driver.findElements(By.tagName("a")).size());
 
+        // limiting webdriver scope by created variable for footer section
         WebElement footerDriver = driver.findElement(By.xpath("//footer[@class='clearfix simple']"));
 
-        // limiting driver scope
         // footerDriver acts like a driver but only for footer section
         System.out.println(footerDriver.findElements(By.tagName("a")).size());
+
+        //limited section of footerDriver
+        WebElement footerSiteLinksDriver = footerDriver.findElement(By.xpath("//ul[@class='footer-site-links']"));
+
+        int numberOfFooterSiteLinks = footerSiteLinksDriver.findElements(By.tagName("a")).size();
+
+        System.out.println(numberOfFooterSiteLinks);
+
+        // click on each link in the footer site links and check if the page are opening
+        // generic solution -> check how many links are present and click in every link individual
+
+        // open 6 links in different tabs
+        for(int i=0; i<numberOfFooterSiteLinks; i++){
+
+            // can click 2 keys in one time ctrl + enter (ctrl + enter = open link in a new tab)
+            String ckickOnLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+
+            footerSiteLinksDriver.findElements(By.tagName("a")).get(i).sendKeys(ckickOnLink);
+        }
     }
 }

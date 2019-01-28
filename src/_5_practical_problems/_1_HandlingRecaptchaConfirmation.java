262f193bdd4efc356821c5a498673386bcfc5ded
Karol Warakomski <k.warakomski1@gmail.com>
2019-01-28 15:46:48 +0100

    problem with select checkbox in frame

--- src/_5_practical_problems/_1_HandlingRecaptchaConfirmation.java
+++ src/_5_practical_problems/_1_HandlingRecaptchaConfirmation.java
@@ -3,6 +3,9 @@ package _5_practical_problems;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class _1_HandlingRecaptchaConfirmation {
 
@@ -10,34 +13,59 @@ public class _1_HandlingRecaptchaConfirmation {
 
         System.setProperty("webdriver.chrome.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/chromedriver_linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
+
+/*      System.setProperty("webdriver.gecko.driver", "/home/karol/ProjectsJAVA/SeleniumTraining/geckodriver-v0.23.0-linux64/geckodriver");
+        WebDriver driver = new FirefoxDriver();*/
         driver.get("https://9gag.com/");
         driver.manage().window().maximize();
 
         driver.findElement(By.xpath("//button[@class='button_button--lgX0P intro_acceptAll--23PPA ']")).click();
         driver.findElement(By.id("jsid-signup-button")).click();
         driver.findElement(By.xpath("//a[text()='Email Address']")).click();
-        driver.switchTo().frame(driver.findElement(By.name("a-1tasl6ahsbrs")));
-        driver.findElement(By.className("recaptcha-checkbox-checkmark")).click();
 
+        WebDriverWait waitingForIframe = new WebDriverWait(driver, 15);
+        waitingForIframe.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.tagName("iframe")));
+
+        int frameNumber = findFrameNumberWithCheckboxElement(driver, By.xpath("//*[@id='recaptcha-anchor']/div[5]"));
+
+        //driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src, 'google.com/recaptcha/api2')]")));
+
+        driver.switchTo().frame(frameNumber);
+
+        WebDriverWait waitingForCheckbox = new WebDriverWait(driver, 10);
+        waitingForCheckbox.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='recaptcha-anchor']/div[5]")));
+
+        driver.findElement(By.xpath("//*[@id='recaptcha-anchor']/div[5]")).click();
     }
 
-    // number of iframe in application is equal to number of iframe tags
-    public void switchToFrame(WebDriver driver) {
+    // I HAVE PROBLEM WITH FINDING CHECKBOX IN CORRECT ?? (I THINK) IFRAME
 
+    public static int findFrameNumberWithCheckboxElement(WebDriver driver, By by) {
+
+        int i;
+
+        // number of iframe in application is equal to number of iframe tags
         int frameCount = driver.findElements(By.tagName("iframe")).size();
 
-        for (int i = 0; i < frameCount; i++) {
+        // using this loop I will search through all the iframes in the page
+        for (i = 0; i < frameCount; i++) {
+
             driver.switchTo().frame(i);
 
             // if 0 - element is not at this iframe || if 1 - element is on the page
-            int countCheckboxeInFrame = driver.findElements(By.className("recaptcha-checkbox-checkmark")).size();
+            int countCheckboxesInFrame = driver.findElements(by).size();
 
-            if(countCheckboxeInFrame>0){
-                driver.findElement(By.className("recaptcha-checkbox-checkmark")).click();
+            if (countCheckboxesInFrame > 0) {
+                //driver.findElement(by).click();
+                break; // when I found earlier the element I quite the loop
+            } else {
+                driver.switchTo().defaultContent();
             }
-
-
         }
 
+        driver.switchTo().defaultContent();
+
+        // return number of iframe that have checkbox that I need
+        return i;
     }
 }
